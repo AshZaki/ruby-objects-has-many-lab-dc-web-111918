0d9152ab204a69bb753ca5b7c77e21e5d8f4a274
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-25 22:34:03 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,9 +20,7 @@ class Artist
   end
 
   def add_song_by_name(song_name)
-    binding.pry
-    new_song = song.new(song_name)
-    new_song.artist = self
+    self.songs << Song.new(song_name)
   end
   
 end
