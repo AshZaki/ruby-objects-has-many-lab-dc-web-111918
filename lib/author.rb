f5f8c29eafeb00e3723d1dc9788af9aa32652584
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-25 22:51:25 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,18 @@
 class Author
   attr_accessor :name, :posts, :author
   
+  @@all =[]
   def initialize(name)
     @name = name
     @posts = []
   end
   
+  def self.all
+    @@all
+  end
+  
   def add_post(title)
+    
     @posts << title
   end
   
